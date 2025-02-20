c0c31a23cea970fe761f9522b76b8da4ad58d6d7
Diego Carvalho <diegocarvalhomr@gmail.com>
2025-02-20 16:40:07 -0300

    Refatoração

--- script.js
+++ script.js
@@ -2,42 +2,66 @@ const formAddTodo = document.querySelector(".form-add-todo");
 const inputSearchTodo = document.querySelector(".form-search input");
 const todosContainer = document.querySelector(".todos-container");
 
-formAddTodo.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const inputValue = event.target.add.value.trim();
+const addTodo = (inputValue) => {
   if (inputValue.length) {
     todosContainer.innerHTML += `
-      <li class="list-group-item d-flex justify-content-between align-items-center">
+      <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
         <span>${inputValue}</span>
-        <i class="far fa-trash-alt delete"></i>
+        <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
       </li>`;
 
     event.target.reset();
   }
+};
+
+formAddTodo.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const inputValue = event.target.add.value.trim();
+  addTodo(inputValue);
 });
+
+const removeTodo = (clickedElement) => {
+  const dataTrashValue = clickedElement.dataset.trash;
+  const todo = document.querySelector(`[data-todo="${dataTrashValue}"]`);
+
+  if (dataTrashValue) {
+    todo.remove();
+  }
+};
 
 todosContainer.addEventListener("click", (event) => {
-  const clicketElement = event.target;
-
-  if (Array.from(clicketElement.classList).includes("delete")) {
-    clicketElement.parentElement.remove();
-  }
+  const clickedElement = event.target;
+  removeTodo(clickedElement);
 });
+
+const filterTodos = (todos, inputValue, returnMachedTodos) =>
+  todos.filter((todo) => {
+    const machedTdodos = todo.textContent.toLowerCase().includes(inputValue);
+    return returnMachedTodos ? machedTdodos : !machedTdodos;
+  });
+
+const manipulateClasses = (todos, classToAdd, classToRemove) => {
+  todos.forEach((todo) => {
+    todo.classList.remove(classToRemove);
+    todo.classList.add(classToAdd);
+  });
+};
+
+const hideTodos = (todos, inputValue) => {
+  const todosToHide = filterTodos(todos, inputValue, false);
+  manipulateClasses(todosToHide, "hidden", "d-flex");
+};
+
+const showTodos = (todos, inputValue) => {
+  const todosToShow = filterTodos(todos, inputValue, true);
+  manipulateClasses(todosToShow, "d-flex", "hidden");
+};
 
 inputSearchTodo.addEventListener("input", (event) => {
   const inputValue = event.target.value.trim().toLowerCase();
-  Array.from(todosContainer.children)
-    .filter((todo) => !todo.textContent.toLowerCase().includes(inputValue))
-    .forEach((todo) => {
-      todo.classList.remove("d-flex");
-      todo.classList.add("hidden");
-    });
+  const todos = Array.from(todosContainer.children);
 
-  Array.from(todosContainer.children)
-    .filter((todo) => todo.textContent.toLowerCase().includes(inputValue))
-    .forEach((todo) => {
-      todo.classList.remove("hidden");
-      todo.classList.add("d-flex");
-    });
+  hideTodos(todos, inputValue);
+  showTodos(todos, inputValue);
 });
